e69cc5782331439cb0b24237982f5205977550ee
ZoeElizabeth <zoe.gelech@gmail.com>
2018-10-09 23:39:19 +0000

    have counter turning red and counting down

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,22 @@
 $(document).ready(function() {
   
-let textarea = $('textarea');
-let counter = textarea.siblings('.counter')
+let textarea = $('textarea')
+let counter = textarea.siblings('.counter');
 
-textarea.on('input', function(event){
+textarea.on('input', function(){
 
-  let textarea= $(this);
+  let textarea = $(this);
   let charInput = textarea.val();
   let currentCount = 140 - charInput.length;
   
+
   counter.text(currentCount);
-  
+
+  if (currentCount <= 0) {
+    counter.css('color', 'red');
+  } else { 
+    counter.css('color', 'black');
+  }
 
 })
 
