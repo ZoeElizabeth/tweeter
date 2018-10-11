e879d723e8e56ab1b4cee515cc47bfadbb554e99
ZoeElizabeth <zoe.gelech@gmail.com>
2018-10-11 23:38:50 +0000

    Preparation for branch merge

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -6,7 +6,7 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      db.collection("tweets").insertOne(newTweet, callback(err));
+      db.collection("tweets").insertOne(newTweet, callback(null, true));
     },
 
     // Get all tweets in `db`, sorted by newest first
