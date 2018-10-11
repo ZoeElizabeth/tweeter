9d1d31af8144af5b5d8dfb2e362946dda8721af7
ZoeElizabeth <zoe.gelech@gmail.com>
2018-10-11 23:32:44 +0000

    Databsse connected and dat-helper modified to support the change and load/save tweets

--- server/index.js
+++ server/index.js
@@ -6,8 +6,8 @@ const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
-const MongoClient   = require("monogodb").MongoClient;
-const MongoDB_URI   = "mongodb://localhost:27017/tweeter";
+const MongoClient   = require("mongodb").MongoClient;
+const MONGODB_URI   = "mongodb://localhost:27017/tweeter";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -20,6 +20,7 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
   }
   console.log(`Connected to mongodb: ${MONGODB_URI}`);
 
+  // console.log(tweets)
 // The in-memory database of tweets. It's a basic object with an array in it.
 // const db = require("./lib/in-memory-db");
 
